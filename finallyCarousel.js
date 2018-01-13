75810955b394c460e1c24ddbf7352f904f15d2f1
xwx <291857648@qq.com>
2018-01-13 21:26:18 +0800

    first commit

---
+++ finallyCarousel.js
@@ -0,0 +1,90 @@
+var $dots = $(".dots .dot");
+var $dotslength = $(".dots .dot").length
+var current = 0
+var $imgbox = $(".imgbox");
+
+makeFakeSlides()
+
+$imgbox.css('transform', 'translateX(-100vw)')
+
+bindEvents()
+
+$(".next").on('click',function(){
+	goToslide(current+1)
+	clearInterval(time)
+})
+$(".pre").on('click',function(){
+	goToslide(current-1)
+	clearInterval(time)
+})
+
+var time = setInterval(function(){
+	goToslide(current+1)	
+},2000)
+
+$(".imgbox").hover(function(){
+	clearInterval(time)
+},function(){
+	time = setInterval(function(){
+		goToslide(current+1)	
+	},2000)
+})
+
+function bindEvents() {
+
+	$(".dots").on('click', 'span', function(e) {
+		let $span = $(e.currentTarget)
+		let index = $span.index();
+		goToslide(index)
+		
+	})
+}
+function goToslide(index){
+	
+	if(index>$dotslength-1){
+		index = 0
+	}else if(index <0){
+		index = $dotslength-1
+	}
+	
+	if(current == $dotslength - 1 && index == 0) {
+				$imgbox.css({
+					transform: `translateX(${-($dotslength+1)*100}vw)`
+				}).one('transitionend',
+					function() {
+						$imgbox.hide()
+							.offset()
+						$imgbox.css({
+								transform: `translateX(${-(index+1)*100}vw)`
+							})
+							.show()
+					}
+				)
+			} else if(current == 0 && index == $dotslength - 1) {
+				$imgbox.css({
+					transform: `translateX(0vw)`
+				}).one('transitionend',
+					function() {
+						$imgbox.hide()
+							.offset()
+						$imgbox.css({
+								transform: `translateX(${-(index+1)*100}vw)`
+							})
+							.show()
+					}
+				)
+			} else {
+				$imgbox.css({
+					transform: `translateX(${-(index+1)*100}vw)`
+				})
+			}
+			current = index	
+}
+
+function makeFakeSlides() {
+	var $firstCopy = $imgbox.find('img').eq(0).clone(true)
+	var $lastCopy = $imgbox.find('img').eq($dots.length - 1).clone(true)
+
+	$imgbox.append($firstCopy)
+	$imgbox.prepend($lastCopy)
+}
